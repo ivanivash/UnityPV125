9901954485ae90ac0501918356bbc72804a4c851
System Administrator <ivanivash>
2023-11-13 20:49:29 +0200

    AppleTree

--- ApplePicker/Assets/Script/AplleTree.cs
+++ ApplePicker/Assets/Script/AplleTree.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class AplleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    //Ігровий обєкт який переміщається
+    public GameObject applePrefab;
+    //Швидкість переміщення
+    public float speed = 1f;
+    // Відстань, яку буде проходить яблуня
+    public float leftAndRightEdge = 10f;
+    // частота зміни яблук
+    public float chanceToChangeDirections = 0.1f;
+    // 1 раз у секунду буде падать яблука
+    public float secondsBetweenAppleDrops = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,8 @@ public class AplleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
     }
 }
