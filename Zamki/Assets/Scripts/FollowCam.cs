7a11dfe7a11589bc198ba2c3cd5a034ee9031b08
System Administrator <ivanivash>
2023-11-24 00:48:10 +0200

    добавлено замок і камера повертається пісял пострілу

--- Zamki/Assets/Scripts/FollowCam.cs
+++ Zamki/Assets/Scripts/FollowCam.cs
@@ -19,9 +19,32 @@ public class FollowCam : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (POI == null) return;
+        // if (POI == null) return;
 
-        Vector3 destination = POI.transform.position;
+        // Vector3 destination = POI.transform.position;
+        Vector3 destination;
+        // якщо не має обʼєкта, то вернути Р:[ 0, 0, 0 ]
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            // отримати позицію обїєкта
+            destination = POI.transform.position;
+            // якщо обʼєк снаряд, то переконуємося, що він зупинився
+            if (POI.tag == "Projectile")
+            {
+                // якщо стоїть на місці (не рухається)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // повернути камеру на початок
+                    POI = null;
+                    return;
+                }
+            }
+
+        }
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
